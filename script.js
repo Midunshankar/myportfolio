8a51fed3129e0421d5bde0dc3cb50b5bda40588d
Midun Shankar K <midunshankar2007@gmail.com>
2026-08-17 15:48:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -488,22 +488,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-const themeToggle = document.querySelector(".theme-toggle");
-
-// Check saved theme
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-}
-
-// Toggle dark/light mode
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-
-    // Save theme
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-});
-    
